82139b4cd0a787997fcd5dd6bde3b3af07465305
Pro-ITDevSuccess <itdevsuccess@gmail.com>
2023-04-11 10:13:47 +0300

    Comprendre les composant

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+  `,
+  styleUrls: []
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent{
+  title = 'Mon Web';
 }
