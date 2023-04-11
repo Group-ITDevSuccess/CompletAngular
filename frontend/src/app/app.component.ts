8cd0dff56a7b8ef2ee891bb7509b8e44da7ec36c
Pro-ITDevSuccess <itdevsuccess@gmail.com>
2023-04-11 10:47:13 +0300

    Les templates (interpolation et integratio de librairie

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>{{title}}</h1>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: []
 })
 export class AppComponent{
---
+++ frontend/src/app/hotel-list/hotel-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-hotel-list',
+  templateUrl: './hotel-list.component.html',
+})
+
+export class HotelListComponent {
+  public title = 'List Hotels'
+}
