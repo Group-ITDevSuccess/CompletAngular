06a94b095c1166a9c3a57c8614236cb177f1893b
Pro-ITDevSuccess <itdevsuccess@gmail.com>
2023-04-13 09:46:01 +0300

    Getter (Get) et Setter (Set) avec Typescript et creation de filtre de liste

--- frontend/src/app/hotel-list/hotel-list.component.ts
+++ frontend/src/app/hotel-list/hotel-list.component.ts
@@ -43,13 +43,35 @@ export class HotelListComponent implements OnInit {
   }];
 
   public showBadge: boolean;
-  public hotelFilter = "mot";
+  private _hotelFilter = "mot";
+  public filteredHotels : IHotel[] = [];
 
   ngOnInit(): void {
-
+    this.filteredHotels = this.hotels;
+    this.hotelFilter = 'mot';
   }
 
   public togglesNewBadge(): void{
     this.showBadge = ! this.showBadge;
   }
+
+  public get hotelFilter(): string{
+    return this._hotelFilter;
+  }
+
+  public set hotelFilter(filter: string){
+    this._hotelFilter = filter;
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter) : this.hotels;
+  }
+
+  private filterHotels(criteria: string): IHotel[]{
+    criteria = criteria.toLocaleLowerCase();
+
+    const res = this.hotels.filter(
+      (hotel : IHotel) => hotel.hotelName.toLocaleLowerCase().indexOf(criteria) != -1
+    );
+
+    return res;
+  }
+
 }
