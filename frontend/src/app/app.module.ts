c2cdcafd2db236bc7832c1b0784bb31595cadef6
Pro-ITDevSuccess <itdevsuccess@gmail.com>
2023-04-17 10:08:54 +0300

    Navigation avec routerLink et router-outlet

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import { HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { ReplaceComma } from './shared/pipes/replace-comma.pipe';
 import { StartRatingComponent } from './shared/components/star-rating/star-rating.component';
+import { HomeComponent } from './home/home.component';
+import { HotelDetailComponent } from './hotel-list/hotel-detail/hotel-detail.component';
 
 registerLocaleData(localeFr, 'fr');
 
@@ -18,12 +21,21 @@ registerLocaleData(localeFr, 'fr');
     AppComponent,
     HotelListComponent,
     ReplaceComma,
-    StartRatingComponent
+    StartRatingComponent,
+    HomeComponent,
+    HotelDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'hotels/:id', component: HotelDetailComponent},
+      {path: 'hotels', component: HotelListComponent},
+      {path: '**', redirectTo: 'home', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
